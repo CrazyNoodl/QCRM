f0482da44143e29f03af631d0d4dea8b036dfaa2
Rudenko Artem <artem.rud@bk.ru>
2020-01-03 10:48:22 +0200

    bg fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const section = document.querySelector('.section');
 let backgroundImages = section.dataset.backgroundImage.split(' ');
 
 function changeBg() {
-  section.style.background = `url(../images/${backgroundImages[Math.floor(Math.random() * 3)]}.jpg) center no-repeat`;
+  section.style.background = `url(./images/${backgroundImages[Math.floor(Math.random() * 3)]}.jpg) center no-repeat`;
 };
 
 setInterval(changeBg, 5000);
