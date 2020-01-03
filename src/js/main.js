8c534b46bea6155096cee5db47ac4b38cf65cea7
Rudenko Artem <artem.rud@bk.ru>
2020-01-03 02:08:41 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// const section = document.querySelector('.section');
+// let backgroundImages = section.dataset.backgroundImage.split(' ');
+
+// function changeBg() {
+//   section.style.background = `url(../images/${backgroundImages[Math.floor(Math.random() * backgroundImages.length)]}.jpg) center no-repeat`;
+// };
+
+// setInterval(changeBg, 5000);
+
